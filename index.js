7912a46184c5ddb07cade99aa5370ff752d21312
hcr5 <157815647+hcr5@users.noreply.github.com>
2025-02-15 16:20:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,53 @@ const canvas = document.getElementById("board")
 const ctx = canvas.getContext("2d")
 const socket = new WebSocket("wss://clouddata.turbowarp.org/")
 
-socket.addEventListener("open", function () {
-    socket.send(JSON.stringify({"method":"handshake","user":"player","project_id":"1133733472"}))
-})
+socket.addEventListener("open", () => { socket.send(JSON.stringify({method:"handshake",user:"player",project_id:"1133733472"})) })
 
-ctx.strokeStyle = "black"
-ctx.lineWidth = 3
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+ctx.lineWidth = 5
+ctx.strokeStyle = "red"
+ctx.lineCap = 'round'
 
 let drawing = false
+let last = null
 
-canvas.addEventListener("mousedown", function() {drawing = true})
-canvas.addEventListener("mouseup", function() {drawing = false})
-canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mousedown", e => {
+  drawing = true
+  ctx.beginPath()
+  ctx.moveTo(e.offsetX, e.offsetY)
+  socket.send(JSON.stringify({method:"set",user:"player",project_id:"1133733472",name:"☁ pos",value:`${e.offsetX}.${e.offsetY}`}))
+})
 
-function draw(event) {
-    if (!drawing) return
+canvas.addEventListener("mouseup", () => {
+  drawing = false
+  last = null
+  socket.send(JSON.stringify({method:"set",user:"player",project_id:"1133733472",name:"☁ pos",value:"-1"}))
+})
 
-    ctx.lineTo(event.offsetX, event.offsetY)
+canvas.addEventListener("mousemove", e => {
+  if (drawing) {
+    ctx.lineTo(e.offsetX, e.offsetY)
     ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(event.offsetX, event.offsetY)
+    socket.send(JSON.stringify({method:"set",user:"player",project_id:"1133733472",name:"☁ pos",value:`${e.offsetX}.${e.offsetY}`}))
+  }
+})
 
-
-    socket.send(JSON.stringify({method: "set", user: "player", project_id: "1133733472", name: "☁ pos", value:`${event.clientX}.${event.clientY}`}))
-}
-
-socket.addEventListener("message", function(event){
-    const x = JSON.parse(event.data).value.split(".")[0]
-    const y = JSON.parse(event.data).value.split(".")[1]
-
-    ctx.lineTo(x, y)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(x, y)
+socket.addEventListener("message", e => {
+  const d = JSON.parse(e.data)
+  const value = d.value
+  if (value === "-1") {
+    last = null
+    return
+  }
+  const [x, y] = value.split(".").map(Number)
+  if (!last) {
+    last = { x, y }
+    return
+  }
+  ctx.beginPath()
+  ctx.moveTo(last.x, last.y)
+  ctx.lineTo(x, y)
+  ctx.stroke()
+  last = { x, y }
 })
